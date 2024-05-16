9674adc8840d023287cdbba2241eb99a40540306
Matthieu <virdouck@gmail.com>
2024-05-16 09:36:19 +0200

    Initial commit with basic Next.js setup and Tailwind CSS integration

---
+++ src/components/Header.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Header: React.FC = () => {
+  return (
+    <header className="bg-[#d99153] p-1 flex justify-between items-center ">
+      <p className="text-sm flex-row-reverse">Bienvenue sur notre site ! 🍔 </p>
+      <div className="flex flex-row ">
+        <input
+          type="text"
+          placeholder="Recherche..."
+          className="flex-row-reverse p-0.5 w-60 text-black text-xs bg-white rounded-lg focus:outline-none focus:shadow-outline"
+        />
+      </div>
+    </header>
+  );
+};
+
+export default Header;
